50f23069cfc740b2046258ba51eba5418c179e70
wangyang <yang.wang@tongdun.cn>
2018-02-28 21:35:43 +0800

    将生成的研报结果按照行业排序

---
+++ Stock/Stocks.py
@@ -0,0 +1,76 @@
+# -*- coding:utf-8 -*-
+
+import os
+import logging
+import urllib.request
+
+'''
+   东方财富 - 数据中心 - 个股资金流 - 沪深A股
+   获取股票的code
+'''
+# 日志
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+logging.basicConfig(filename='parse.log', level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+
+class Stocks:
+    # p 为页数， ps为每页条数.
+    url_base = 'http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx/JS.aspx?type=ct&st=(BalFlowMain)&sr=-1&p=$$&ps=100&token=894050c76af8597a853f5b408b759f5d&cmd=C._A&sty=DCFFITA&rt=50651072'
+
+    local_file = 'stock.data'
+
+    @classmethod
+    def parse_stock_page(self, page):
+        # 去掉 ([  ])
+        page = page.replace('([', '').replace(')]', '')
+        # 转为tuple
+        page_groups = page.split('","')
+        result = {}
+        for stock in page_groups:
+            code = stock.split(',')[1]
+            name = stock.split(',')[2]
+            result[code] = name
+        return result
+
+    @classmethod
+    def persist_stocks_url(self):
+        file_handle = open(self.local_file, 'w')
+        for i in range(1, 50):
+            url = self.url_base.replace('$$', str(i))
+            request = urllib.request.Request(url)
+            response = urllib.request.urlopen(request)
+            stocks_page = response.read().decode('utf-8')
+            stock_dict = self.parse_stock_page(stocks_page)
+            for (code, name) in stock_dict.items():
+                file_handle.write(code + "," + name + "\n")
+            file_handle.flush()
+            # 最后不满100条时退出执行.
+            if (len(stock_dict) < 100):
+                logging.info("退出 persist_stocks_url, 当前页数: " + str(i))
+                break
+        file_handle.close()
+
+    @classmethod
+    def get_code_from_file(self):
+        if (not os.path.isfile(self.local_file)):
+            logging.info(self.local_file + " 文件不存在")
+            return None
+        file_handle = open('stock.data', 'r')
+        stocks_list = file_handle.readlines()
+        result = []
+        for stock in stocks_list:
+            result.append(stock.split(',')[0])
+        return result
+
+    @classmethod
+    def get_code_from_url(self):
+        self.persist_stocks_url()
+        return self.get_code_from_file()
+
+    @classmethod
+    def get_codes(self):
+        codes = self.get_code_from_file()
+        if(not codes):
+            codes = self.get_code_from_url()
+        return codes
--- Stock/parse_research_report.py
+++ Stock/parse_research_report.py
@@ -1,2 +1,156 @@
 # -*- coding:utf-8 -*-
 
+import urllib.request
+import json
+from Stocks import *
+from datetime import date
+from bs4 import BeautifulSoup
+
+key_words = ['龙头', '领先', '行业第']
+
+# 为了获取股票所属行业
+industry_url = 'http://data.eastmoney.com/report/$$.html'
+
+research_report_url = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&ps=50&p=1&code=$$&rt=50652254'
+
+research_result_file = 'research_report.data'
+
+
+# 获取需要的文件
+def get_research_files(code):
+    today = date.today().isoformat()
+    json_file = today + '/' + code + '.data'
+    html_file = today + '/' + code + '.html'
+    # 本地存在文件，则读取本地的.
+    if (os.path.isfile(json_file) and os.path.isfile(html_file)):
+        file_handle = open(json_file, 'r')
+        return file_handle.read()
+    if (not os.path.isdir(today)):
+        os.mkdir(today)
+
+    json_url = research_report_url.replace('$$', code)
+    persist_file(json_url, json_file, 'utf-8')
+
+    html_url = industry_url.replace('$$', code)
+    persist_file(html_url, html_file, 'GBK')
+
+    file_handle = open(json_file, 'r')
+    return file_handle.read()
+
+
+# 获取指定url, 保存至本地
+def persist_file(url, file, encoding):
+    try:
+        # 构建请求的request
+        request = urllib.request.Request(url)
+        # 利用urlopen获取页面代码
+        response = urllib.request.urlopen(request)
+        # 将页面转换编码
+        content = response.read().decode(encoding)
+
+        # 保存至文件
+        file_handle = open(file, 'w')
+        file_handle.write(content)
+        file_handle.close()
+        return content
+    except urllib.request.URLError as e:
+        if hasattr(e, "reason"):
+            print("连接失败,错误原因", e.reason)
+            return None
+
+
+# 获取所有的code
+def get_codes():
+    codes = Stocks.get_codes()
+    logging.info("获取code数: " + str(len(codes)))
+    return codes
+
+
+# 解析研报
+def parse_research_report(code):
+    research_json = get_research_files(code)
+    # 不存在研报时,json: ([{stats:false}])
+    if (not research_json or research_json == '([{stats:false}])'):
+        logging.info('不存在研报: ' + code)
+        return
+    # 去掉前后()
+    research_json = research_json[1:-1]
+
+    all_reports = json.loads(research_json)
+    if (not all_reports):
+        logging.info('不存在研报: ' + code)
+        return
+    calc_dict = {}
+    existed = False
+    # 初始化
+    for key_word in key_words:
+        calc_dict[key_word] = 0
+    for report in all_reports:
+        title = report["title"]
+        for key_word in key_words:
+            if (title.count(key_word) > 0):
+                calc_dict[key_word] = calc_dict[key_word] + 1
+                existed = True
+    # 没有找到任何一个关键词时，直接退出.
+    if(not existed):
+        return
+
+    # soup
+    today = date.today().isoformat()
+    html_file_path = today + '/' + code + '.html'
+    if (not os.path.isfile(html_file_path)):
+        return
+    html_file = open(html_file_path, 'r', encoding='utf-8')
+    soup = BeautifulSoup(html_file, 'html5lib')  # html.parser   html5lib  lxml
+
+    # 名称
+    stock_name_long = soup.select('#s1-tab > li.at')[0].string
+    stock_name = stock_name_long[0:stock_name_long.find('(')]
+
+    # 行业
+    industry = soup.select('#s1-tab > li:nth-of-type(2)')[0].string.replace('研报', '')
+
+    # 保存至文件
+    today = date.today().isoformat()
+    file_path = today + '/' + research_result_file
+    file_handle = open(file_path, 'a')
+    content = industry + ',' + code + ',' + stock_name + ',' + str(calc_dict) + "\n"
+    file_handle.write(content)
+    file_handle.close()
+
+def sort_file(file_path):
+    f = open(file_path)
+    result = []
+    iter_f = iter(f)  # 用迭代器循环访问文件中的每一行
+    for line in iter_f:
+        result.append(line)
+    f.close()
+    result.sort()
+    f = open(file_path, 'w')
+    f.writelines(result)
+    f.close()
+
+def main():
+    codes = get_codes()
+    # codes = ['002707']
+    index = 0;
+    # 先删掉
+    today = date.today().isoformat()
+    file_path = today + '/' + research_result_file
+    if (os.path.isfile(file_path)):
+        os.remove(file_path)
+    for code in codes:
+        index += 1
+        logging.info("正在处理: " + str(index) + " " + code)
+        parse_research_report(code)
+    # 对文件排序
+    today = date.today().isoformat()
+    file_path = today + '/' + research_result_file
+    sort_file(file_path)
+
+
+# get_codes()
+
+# print(getPage())
+
+main()
